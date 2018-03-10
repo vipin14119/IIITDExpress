c6e9ed131d3c52b2d41a3a626ca78453f1bf17d7
vipin14119 <vipin14119@iiitd.ac.in>
2018-03-10 08:17:43 +0530

    Commiting today's final change

--- app/src/main/java/com/dhcs/vipin/iiitdexpress/DashboardActivity.java
+++ app/src/main/java/com/dhcs/vipin/iiitdexpress/DashboardActivity.java
@@ -1,7 +1,9 @@
 package com.dhcs.vipin.iiitdexpress;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,5 +11,36 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+    }
+
+    public void startActivityMessMenu(View view){
+        Intent intent = new Intent(this, ViewPagerMessMenuActivity.class);
+        startActivity(intent);
+    }
+
+    public void startActivityTimeTable(View view){
+        Intent intent = new Intent(this, ViewPagerTimeTableActivity.class);
+        startActivity(intent);
+    }
+
+    public void startActivitySilencio(View view){
+        Intent intent = new Intent(this, SilencioActivity.class);
+        startActivity(intent);
+    }
+
+    public void startActivityDirectory(View view){
+        Intent intent = new Intent(this, DirectoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void startActivityFaculty(View view){
+        Intent intent = new Intent(this, FacultyActivity.class);
+        startActivity(intent);
+    }
+
+    public void startActivityPlacement(View view){
+        Intent intent = new Intent(this, PlacementActivity.class);
+        startActivity(intent);
     }
 }
