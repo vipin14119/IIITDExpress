5d573bf5665418cdefccdddc965835defac60d44
vipin14119 <vipin14119@iiitd.ac.in>
2018-04-19 09:00:25 +0530

    config file updated

---
+++ app/src/main/java/com/dhcs/vipin/iiitdexpress/Config.java
@@ -0,0 +1,10 @@
+package com.dhcs.vipin.iiitdexpress;
+
+/**
+ * Created by vipin on 19/04/18.
+ */
+
+public class Config {
+    public static String USERNAME = "admin";
+    public static String PASSWORD = "pass1234";
+}
